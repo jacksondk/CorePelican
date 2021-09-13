c3b1d4acc73627e8a301c9824b80f27606cf0aed
Michael Jacobsen <mic.jacobsen@gmail.com>
2021-09-13 20:25:33 +0200

    Use configuration file. Iterate over all articles in directory. Generate target file names that match old Pelican format.

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Markdig;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
+using RazorEngineCore;
 
 namespace CorePelican
 {
@@ -31,21 +33,54 @@ namespace CorePelican
             var articleLines = content.AsSpan().Slice(lineIndex + 1).ToArray();
 
             var document = Markdig.Markdown.Parse(string.Join("\n", articleLines));
-            
+
             // Finding links
             //foreach (var link in document.Descendants<LinkInline>())
             //    Console.WriteLine(link.Url);
 
+            DateTime.TryParseExact(dict["Date"], "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out var parsedDate);
             return new Article
             {
                 Title = dict["Title"],
+                TimeStamp = parsedDate,
                 HtmlContent = document.ToHtml()
             };
         }
 
-        static void WriteArticle(Article article)
+        public class ArticleTransformer
         {
+            private RazorEngine _engine;
+            private IRazorEngineCompiledTemplate _compiledTemplate;
 
+            public ArticleTransformer()
+            {
+                
+            }
+
+            static public ArticleTransformer SetupTemplate(Configuration configuration)
+            {
+                var transformer = new ArticleTransformer();
+                transformer._engine = new RazorEngineCore.RazorEngine();
+                var template = File.ReadAllText(Path.Combine(configuration.TemplatePath, "article.html"));
+                transformer._compiledTemplate = transformer._engine.Compile(template);
+                return transformer;
+            }
+
+            public void WriteArticle(string outputFileName, Article article, Configuration conf)
+            {
+                string result = _compiledTemplate.Run(
+                    new
+                    {
+                        Title = article.Title,
+                        Content = article.HtmlContent
+                    }
+                    );
+                File.WriteAllText(outputFileName, result);
+            }
+        }
+
+        static void WriteArticle(string outputFileName, Article article, Configuration conf)
+        {
             RazorEngineCore.RazorEngine engine = new RazorEngineCore.RazorEngine();
             var template = File.ReadAllText("template/article.html");
             RazorEngineCore.IRazorEngineCompiledTemplate razorEngineCompiledTemplate = engine.Compile(template);
@@ -61,11 +96,46 @@ namespace CorePelican
 
         }
 
+        public class Configuration
+        {
+            public string ArticlePath { get; set; }
+            public string StaticFilesPath { get; set; }
+            public string TemplatePath { get; set; }
+
+            public string OutputPath { get; set; }
+        }
 
         static void Main(string[] args)
         {
-            var article = ParseContent(args[0]);
-            WriteArticle(article);
+            var configFile = File.ReadAllText(args[0]);
+            var config = System.Text.Json.JsonSerializer.Deserialize<Configuration>(configFile);
+
+            var files = Directory.GetFiles(config.ArticlePath, "*.md");
+            var articles = files.Select(f =>
+            {
+                return ParseContent(f);
+            });
+
+            if (Directory.Exists(config.OutputPath) is false)
+            {
+                Directory.CreateDirectory(config.OutputPath);
+            }
+            var articleCreator = ArticleTransformer.SetupTemplate(config);
+            foreach(var article in articles)
+            {
+                var htmlFileName = article.Title
+                    .Replace(' ', '-')
+                    .Replace("?","")
+                    .ToLower() + ".html";
+                
+                var ymdPath = article.TimeStamp.ToString("yyyy/MM/dd");
+                var totalPath = Path.Combine(config.OutputPath, ymdPath);
+                if (Directory.Exists(totalPath) is false)
+                    Directory.CreateDirectory(totalPath);
+                var outputFileName = Path.Combine(totalPath, htmlFileName);
+                articleCreator.WriteArticle(outputFileName, article, config);
+            }
+                        
         }
     }
 }
